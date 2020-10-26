e8300fb60f36603ba25d6ae322e225a8750481d0
Huynh Du Luong <huynhduluong@gmail.com>
2020-10-26 13:39:30 +0700

    Updates

---
+++ precache-manifest.9b08a214c639f2c3d933c70a1e7ca779.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7524c0fe69aecf986c6323215d5e9a73",
+    "url": "/game-webapp/index.html"
+  },
+  {
+    "revision": "5ecc28c8a7fecd7a2ef9",
+    "url": "/game-webapp/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e6ce04063b354038081f",
+    "url": "/game-webapp/static/css/main.f53f5175.chunk.css"
+  },
+  {
+    "revision": "5ecc28c8a7fecd7a2ef9",
+    "url": "/game-webapp/static/js/2.1e3584f8.chunk.js"
+  },
+  {
+    "revision": "54e9f1f53167e769a804ac78b76052ab",
+    "url": "/game-webapp/static/js/2.1e3584f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6ce04063b354038081f",
+    "url": "/game-webapp/static/js/main.414a54b9.chunk.js"
+  },
+  {
+    "revision": "2e60f25c48e6c7591a08",
+    "url": "/game-webapp/static/js/runtime-main.602fd787.js"
+  },
+  {
+    "revision": "1716f3f87bf9af14a763249e77f202da",
+    "url": "/game-webapp/static/media/Pony.1716f3f8.ttf"
+  },
+  {
+    "revision": "e5bbccbf5b4ad91f4ef1d02810ec01f0",
+    "url": "/game-webapp/static/media/bgGame.e5bbccbf.jpg"
+  },
+  {
+    "revision": "99715b85c43815dc2254c4c0b11b21e5",
+    "url": "/game-webapp/static/media/bgHome.99715b85.jpg"
+  }
+]);
